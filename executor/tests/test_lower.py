032c67e96e75e0e9b04d7edeb132a16eb02a080a
boneykkudyjose <boney.jose@falkonry.com>
2022-07-07 12:24:54 -0700

    init

---
+++ executor/tests/test_lower.py
@@ -0,0 +1,7 @@
+def lower_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.lower()
+
+def test_capitalize_string():
+    assert lower_string('Test') == 'test'
